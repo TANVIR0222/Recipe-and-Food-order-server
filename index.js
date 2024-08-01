03147eece8b84c2ba09b776d685d1c15a7917166
TANVIR0222 <tanvirislam3912@gmail.com>
2024-08-01 23:46:20 +0600

    import dotenv

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+require('dotenv').config()
+
+
 const app = express();
 const port = process.env.PORT || 5000;
+dotenv.config();
 
-app.get("/", (req, res) => {
-  res.send("Hello Server");
-});
+
+async function main() {
+  await mongoose.connect(
+    process.env.MONGODB
+  );
+
+  app.get("/", (req, res) => {
+    res.send("Hello Server");
+  });
+}
+
+main().then(()=>
+    console.log('mongoDb')
+).catch((err) => console.log(err));
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+// yO6IQ6GsqOELzR9S tanvirislam3912
