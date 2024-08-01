d58655e1fbddb9de528b9ced9eebef2d1e0e77c4
TANVIR0222 <tanvirislam3912@gmail.com>
2024-08-01 23:46:37 +0600

    create item model

---
+++ src/Models/itemModel.js
@@ -0,0 +1,84 @@
+import mongoose from "mongoose";
+
+const ingredientsItem = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+});
+
+const commentItem = new mongoose.Schema({
+  user: {
+    type: String,
+    required: true,
+  },
+  comment: {
+    type: String,
+    required: true,
+  },
+});
+const moreItem = new mongoose.Schema({
+  prep_time: {
+    type: String,
+    required: true,
+  },
+  cook_time: {
+    type: String,
+    required: true,
+  },
+  servings: {
+    type: String,
+    required: true,
+  },
+  difficulty: {
+    type: String,
+    required: true,
+  },
+  source: {
+    type: String,
+    required: true,
+  },
+});
+
+const itemSchema = new mongoose.Schema({
+  menuId: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  thumbnail_image: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  instructions: {
+    type: String,
+    required: true,
+  },
+  tags: [String],
+  ingredients: {
+    type: [ingredientsItem],
+    required: true,
+  },
+
+  comments: {
+    type: [commentItem],
+    required: true,
+  },
+  more: {
+    type: [moreItem],
+    required: true,
+  },
+});
+
+export const Item = mongoose.model('Item', itemSchema)
